4a2b109c295c1768a7370482c8b77cf74a75b4b6
candystarr1991 <candygold19@gmail.com>
2022-06-03 06:42:53 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,11 @@
  *
  *  Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	int a;
-        long int b;
-        long long int c;
+	long int b;
+	long long int c;
 	char d;
 	float f;
 
