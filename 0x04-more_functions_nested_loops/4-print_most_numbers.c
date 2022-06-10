14c093752a5683b45484738abe74373548eea192
candystarr1991 <candygold19@gmail.com>
2022-06-10 04:33:24 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_most_numbers -> this prints most numbers
+ */
+void print_most_numbers(void)
+{
+	int ch;
+
+	for (ch = 48; ch < 58; ch++)
+	{
+		if (ch != 50 && ch != 52)
+			_putchar(ch);
+	}
+	_putchar('\n');
+}
