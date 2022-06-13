fb0769ddf8a9af2ae370a89255384733b6f1f990
candystarr1991 <candygold19@gmail.com>
2022-06-13 08:09:14 -0700

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
-#include <main.h>
+#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * reset_to_98 - reset argument to 98
+ * @n: pointer
+ * Return: void
  */
 void reset_to_98(int *n)
 {
